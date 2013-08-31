79b76027309568962c8020ff6d7326104fd54638
timosis <gaetan.bahl@gmail.com>
2013-08-31 20:24:50 +0200

    basic queues

---
+++ fifo.ml
@@ -0,0 +1,13 @@
+(*FIFOOOOO*)
+
+let hd = List.hd;;
+let tl = List.tl;;
+
+type 'a queue =  'a list ref;;
+let add x q = q := x::!q ; x ;;
+let pop = function
+ q when !q = [] -> failwith "empty queue"
+| q -> q := List.rev !q ; let x = hd !q in q := (List.rev (tl !q)) ; x;;
+let newq () = ref [];;
+
+
