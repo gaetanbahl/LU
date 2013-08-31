4662f9c3923b8c5204c7c6176caaacb5d412c17c
timosis <gaetan.bahl@gmail.com>
2013-08-31 20:30:01 +0200

    module !

--- fifo.ml
+++ fifo.ml
@@ -1,13 +1,15 @@
 (*FIFOOOOO*)
 
-type 'a queue =  'a list ref;;
+module Q = struct
+    type 'a queue =  'a list ref;;
 
-let add x q = q := x::!q ; x ;;
+    let add x q = q := x::!q ; x ;;
 
-let pop = function q when !q = [] -> failwith "empty queue"
-| q -> q := List.rev !q ;
-    let x = hd !q in q := (List.rev (tl !q)) ; x;;
+    let pop = function q when !q = [] -> failwith "empty queue"
+        | q -> q := List.rev !q ;
+            let x = hd !q in q := (List.rev (tl !q)) ; x;;
 
-let newq () = ref [];;
+    let newq () = ref [];;
 
+end ;;
 
